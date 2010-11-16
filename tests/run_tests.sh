93eee09a21e80c21710e7f9b72b5aedd72fe0c02
Sanjoy Das <sanjoy@playingwithpointers.com>
2010-11-16 14:22:34 +0530

    Added CFLAGS.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 CC='cc'
+CFLAGS='-g -O0'
 TEST_BINARIES="test_binaries"
 
 if [ ! -d $TEST_BINARIES ]; then
@@ -10,6 +11,6 @@ fi
 for file in *.c; do
 	file_root="`basename $file .c`"
 	echo "Running $file_root"
-	$CC $file -I. -I../src ../src/cmd_args.c -DCMD_ARGS_OPTION_FILE="\"$file_root.opt\"" -o "$TEST_BINARIES/$file_root"
+	$CC $CFLAGS $file -I. -I../src ../src/cmd_args.c -DCMD_ARGS_OPTION_FILE="\"$file_root.opt\"" -o "$TEST_BINARIES/$file_root"
 	$TEST_BINARIES/$file_root
 done
