9c218e79c1e746f04ed4fa4a15d01e6605975149
Sanjoy Das <sanjoy@playingwithpointers.com>
2011-01-17 13:05:33 +0530

    Cosmetic style changes.

--- tests/test.c
+++ tests/test.c
@@ -1,14 +1,16 @@
 #include "test.h"
 
-int almost_equal_eps(double x, double y, double eps)
+int
+almost_equal_eps (double x, double y, double eps)
 {
     if (x == 0) return fabs(x) < eps;
     if (y == 0) return fabs(y) < eps;
-    double max = (fabs(x) > fabs(y)) ? fabs(x) : fabs(y);
-    return fabs(x - y)/max < eps;
+    double max = (fabs(x) > fabs(y)) ? fabs (x) : fabs (y);
+    return fabs (x - y)/max < eps;
 }
 
-int almost_equal(double x, double y)
+int
+almost_equal (double x, double y)
 {
     return almost_equal_eps(x, y, 1e-7);
 }
