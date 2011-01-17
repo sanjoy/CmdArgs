cbde02ec6f51879d45f08da65b76752c0df2a1e3
Sanjoy Das <sanjoy@playingwithpointers.com>
2011-01-17 13:06:48 +0530

    Move stuff around so that
     1. ./run_tests.sh works.
     2. There is not circular dependency.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# If this is invoked directly, run make first
+# to make sure that the binaries are up-to-date
+
+if [ "$#" -eq "0" ]; then
+	make
+fi
+
 BINDIR=test_binaries
 for file in test_*.c; do
 	file_root="`basename $file .c`"
